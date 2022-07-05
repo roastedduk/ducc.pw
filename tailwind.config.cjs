189133120c9c9d8f8a7fda0a926c97962dffea8a
RoastedDuck <roastedducc@gmail.com>
2022-07-05 20:11:35 +0700

    feat: Init

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Plus Jakarta Sans', ...defaultTheme.fontFamily.sans],
+      }
+    },
+  },
+  plugins: [],
+}
