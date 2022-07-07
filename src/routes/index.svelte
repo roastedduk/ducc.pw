538a95896a2b45c1bb1158ee499d41e1fc543143
RoastedDuck <roastedducc@gmail.com>
2022-07-08 01:28:51 +0700

    feat: Index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,16 +3,29 @@
 </script>
 
 <script lang="ts">
+	import Icon from '@iconify/svelte';
 </script>
 
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
 </svelte:head>
-
-<section>
-	Hello
+<section class="h-screen flex justify-center items-center">
+	<div class="flex flex-col w-screen items-center space-y-8 max-w-md md:max-w-none">
+		<h1 class="text-center">Quack! RoastedDuck here.</h1>
+		<div class="flex space-x-4">
+			<a href="https://github.com/roastedduk" class="icon"><Icon icon="tabler:brand-github" width="32"/></a>
+			<a href="https://twitter.com/roastedduk" class="icon"><Icon icon="tabler:brand-twitter" width="32"/></a>
+			<a href="https://discordapp.com/users/297958693293654016" class="icon"><Icon icon="tabler:brand-discord" width="32"/></a>
+			<a href="https://t.me/roasted_duck" class="icon"><Icon icon="tabler:brand-telegram" width="32"/></a>
+		</div>
+		<div>
+			There's more to come!
+		</div>
+	</div>
 </section>
 
 <style>
+	.icon {
+		@apply hover:scale-110 transition-all hover:bg-black dark:hover:bg-white hover:bg-opacity-10 rounded-full p-3;
+	}
 </style>
