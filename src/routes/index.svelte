6a6fad713bdac6e84e02da3b6dc5dfa4cb405c0e
RoastedDuck <roastedducc@gmail.com>
2022-07-08 01:53:04 +0700

    fix: Icons not showing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,16 @@
 </script>
 
 <script lang="ts">
-	import Icon from '@iconify/svelte';
+	import Icon, { addIcon } from '@iconify/svelte/dist/OfflineIcon.svelte';
+	
+	import github from '@iconify-icons/tabler/brand-github.js';
+	import twitter from '@iconify-icons/tabler/brand-twitter.js';
+	import discord from '@iconify-icons/tabler/brand-discord.js';
+	import telegram from '@iconify-icons/tabler/brand-telegram.js';
+	addIcon('github', github);
+	addIcon('twitter', twitter);
+	addIcon('discord', discord);
+	addIcon('telegram', telegram);
 </script>
 
 <svelte:head>
@@ -13,10 +22,10 @@
 	<div class="flex flex-col w-screen items-center space-y-8 max-w-md md:max-w-none">
 		<h1 class="text-center">Quack! RoastedDuck here.</h1>
 		<div class="flex space-x-4">
-			<a href="https://github.com/roastedduk" class="icon"><Icon icon="tabler:brand-github" width="32"/></a>
-			<a href="https://twitter.com/roastedduk" class="icon"><Icon icon="tabler:brand-twitter" width="32"/></a>
-			<a href="https://discordapp.com/users/297958693293654016" class="icon"><Icon icon="tabler:brand-discord" width="32"/></a>
-			<a href="https://t.me/roasted_duck" class="icon"><Icon icon="tabler:brand-telegram" width="32"/></a>
+			<a href="https://github.com/roastedduk" class="icon"><Icon icon="github" width="32"/></a>
+			<a href="https://twitter.com/roastedduk" class="icon"><Icon icon="twitter" width="32"/></a>
+			<a href="https://discordapp.com/users/297958693293654016" class="icon"><Icon icon="discord" width="32"/></a>
+			<a href="https://t.me/roasted_duck" class="icon"><Icon icon="telegram" width="32"/></a>
 		</div>
 		<div>
 			There's more to come!
