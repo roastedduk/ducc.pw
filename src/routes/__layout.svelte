67b9ad13ef6e95b9acf70b8c338c77d5ca2ed838
RoastedDuck <roastedducc@gmail.com>
2022-07-08 02:07:32 +0700

    fix: Font not loading

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import "../app.css";
 	import "@fontsource/plus-jakarta-sans/variable.css";
+	import "@fontsource/plus-jakarta-sans/400.css";
+	import "@fontsource/plus-jakarta-sans/700.css";
 </script>
 
 <main>
